536f612af4019433ae13c569ef69f2aded29e3b0
4love <xzz_jsd1603@163.com>
2019-04-29 21:35:55 +0800

    init

---
+++ src/main/java/com/love4/springboot/shiroall/ShiroallApplication.java
@@ -0,0 +1,13 @@
+package com.love4.springboot.shiroall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/love4/springboot/shiroall/ShiroallApplicationTests.java
@@ -0,0 +1,16 @@
+package com.love4.springboot.shiroall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShiroallApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
